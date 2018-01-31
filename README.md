9b528aeed37d0eb9986e6230dc7f088a5139acbe
Shubham Chaudhary <me@shubhamchaudhary.in>
2018-01-31 12:05:12 +0000

    Allow users to change port number

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ This is a demo of how to use dockerized nginx to proxy requests from one host to
 
 ### Example
 Use nginx to proxy requests to an idea internal company license server.
+
+```bash
+# Install docker
+./run.sh PORT_NUMBER
+# e.g.
+./run.sh 9000
+```
--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 container_name='idea_server'
+port_number=${1}
+docker stop ${container_name} || true
 docker rm ${container_name} || true
-docker run -d --name ${container_name} -v $PWD:/etc/nginx/conf.d/ -p 39015:80 nginx:1.13
+docker run -d --name ${container_name} -v $PWD:/etc/nginx/conf.d/ -p ${port_number}:80 nginx:1.13
